9200ec5b93d41147ab6c10fe2c08de4f9082d823
Nhan Nguyen <nhannt22@hdbank.com.vn>
2024-08-18 23:37:01 +0700

    Complete lession 77 - OOP Part 1 - Inheritance
    Learning course "Java 17 Masterclass: Start coding in 2024"

---
+++ java-learning-masterclass/Classes/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+//        car.describeCar();
+
+        System.out.println("Make: " + car.getMake());
+        System.out.println("Model: " + car.getModel());
+    }
+}
